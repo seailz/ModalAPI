eca0390fe9f21db5a70f357699aefaaf17d28759
Seailz <81972974+IoyoCode@users.noreply.github.com>
2022-07-04 12:25:48 +0100

    license: Added license to the top of every class

--- src/main/java/com/seailz/modalapi/controller/ModalManager.java
+++ src/main/java/com/seailz/modalapi/controller/ModalManager.java
@@ -1,3 +1,21 @@
+/**
+ * MODAL API - Interact with JDA modals with ease.
+ * Copyright (C) 2022 Seailz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.seailz.modalapi.controller;
 
 import com.seailz.modalapi.Modal;
@@ -5,12 +23,10 @@ import lombok.Getter;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
-import net.dv8tion.jda.api.interactions.Interaction;
 import net.dv8tion.jda.api.interactions.components.ActionRow;
 import net.dv8tion.jda.api.interactions.components.ItemComponent;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
 
 /**
